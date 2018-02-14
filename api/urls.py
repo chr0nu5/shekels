8e8b1a097ab40b66f647e2d1b57fd144d4b48726
Jonathan Querubina <jotadev@me.com>
2018-02-14 20:41:00 -0200

    Fix requirements and create initial migrations

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,8 @@
-from authentication import views as auth_views
-from callback import views as callback_views
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from djoser import urls
-from djoser import views as djoser_views
-from djoser.urls import authtoken
-from extract import views as extract_views
 from oauth import views as oauth_views
-from plans import views as plan_views
-from rentability import views as rentability_views
-from sales import views as sales_views
-from simulator import views as simulator_views
-from swagger import views as swagger_views
-
-from rest_framework_expiring_authtoken import views
 
 admin.autodiscover()
 
