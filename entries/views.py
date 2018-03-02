a1041d64e49764d169501ea3ba87ed92e276a71b
Jonathan Querubina <jotadev@me.com>
2018-03-02 17:13:14 -0300

    Update query for entries

--- entries/views.py
+++ entries/views.py
@@ -72,7 +72,7 @@ class ListEntriesView(views.APIView):
         month = self.kwargs.get("month", None)
         day = self.kwargs.get("day", None)
 
-        entries = Entry.objects.filter(client=client)
+        entries = Entry.objects.filter(user=client)
         balance = 0.00
 
         all_entries = entries
