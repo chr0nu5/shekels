00ef50a43b9dd12cccef820b8405eafd902e1de8
Jonathan Querubina <jotadev@me.com>
2019-02-10 12:15:06 -0200

    Force https

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
+import logging
 import os
 
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 
 def index(request):
+
+    full_url = request.build_absolute_uri()
+    if "shekels" in full_url:
+        if "https" not in full_url:
+            full_url = full_url.replace("http", "https")
+            return redirect(full_url)
+
     context = {
         "key": os.environ["API_KEY"],
         "sec": os.environ["API_SEC"]
